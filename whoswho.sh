6ae8b7a56a1e67ec21e95f34f8775bedc9453ee4
ishtaar <ishtaar.desravines@gmail.com>
2022-08-16 20:35:10 -0400

    ishtaar's second commit

--- whoswho.sh
+++ whoswho.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-..
 
-
-
-
-var2="Hotpot is my favorite type of meal \nFall is my favorite season \nBlack is my favorite color 
+var2="Hotpot is my favorite type of meal \nFall is my favorite season \nBlack is my favorite color
+suborna="I love long walks and connecting with the nature. I have a cat. My Favorite food is thai."
