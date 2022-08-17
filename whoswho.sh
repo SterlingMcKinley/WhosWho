73039161f8c566a206c5bf0f6552d20a59b6086d
suborna <subornadnath@gmail.com>
2022-08-16 21:25:35 -0400

    fixed variable ayo. It had spaces btw name and = sign

--- whoswho.sh
+++ whoswho.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-ayo = "Hi. I have lived in 4 countries. My best cuisine is indian food. My favourite hobbies are Kayaking, gaming and photography."
+ayo="Hi. I have lived in 4 countries. My best cuisine is indian food. My favourite hobbies are Kayaking, gaming and photography."
 ishtaar="Hotpot is my favorite type of meal \nFall is my favorite season \nBlack is my favorite color."
 suborna="I love long walks and connecting with the nature. I have a cat. My Favorite food is thai."
 sterling="I love ancient Egyptian culture & civilization.\nI want to retire in Miami, FL\nIwant to be a DevOps Engineer."
+
+
