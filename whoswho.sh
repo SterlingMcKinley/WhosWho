b599afc1d6eb3648a6023ba9a43ae9e384f0861e
SterlingMcKinley <91057035+SterlingMcKinley@users.noreply.github.com>
2022-08-16 19:57:20 -0500

    updating variable names

--- whoswho.sh
+++ whoswho.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 
-var1 = "Hi. I have lived in 4 countries. My best cuisine is indian food. My favourite hobbies are Kayaking, gaming and photography."
-
-var4=I love ancient Egyptian culture & civilization.\nI want to retire in Miami, FL\nIwant to be a DevOps Engineer.
-var2="Hotpot is my favorite type of meal \nFall is my favorite season \nBlack is my favorite color
-
+ayo = "Hi. I have lived in 4 countries. My best cuisine is indian food. My favourite hobbies are Kayaking, gaming and photography."
+ishtaar="Hotpot is my favorite type of meal \nFall is my favorite season \nBlack is my favorite color."
 suborna="I love long walks and connecting with the nature. I have a cat. My Favorite food is thai."
-
+sterling="I love ancient Egyptian culture & civilization.\nI want to retire in Miami, FL\nIwant to be a DevOps Engineer."
